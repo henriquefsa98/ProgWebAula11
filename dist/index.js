82e3214a9ad78d6473d45ee0b1d478dca160dd15
henriquefsa98 <60986807+henriquefsa98@users.noreply.github.com>
2023-07-06 23:17:55 +0000

    await complete

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+async function loadCode(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const text = await response.text();
+        const codeNode = document.querySelector('.code-block');
+        if (codeNode) {
+            codeNode.textContent = text;
+        }
+    }
+    else {
+        throw new Error('Failed to download file: ' + response.status.toString());
+    }
+}
+function main() {
+    const links = document.querySelectorAll('a');
+    links.forEach((link) => {
+        link.addEventListener('click', async (ev) => {
+            await loadCode(link.getAttribute('data-src') || '');
+        });
+    });
+}
+main();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 async function loadCode(url: string){
     const response = await fetch(url)
+
+    if(response.ok){
+        const text = await response.text()
+        const codeNode = document.querySelector('.code-block')
+
+        if(codeNode){
+            codeNode.textContent = text
+        }
+    }
+    else{
+        throw new Error('Failed to download file: ' + response.status.toString())
+    }
 }
 
 
@@ -8,10 +20,11 @@ function main(){
     const links = document.querySelectorAll('a')
 
     links.forEach((link) => {
-        link.addEventListener('click', (ev) => {
-            loadCode(link.getAttribute('data-src') || '')
+        link.addEventListener('click', async (ev) => {
+            await loadCode(link.getAttribute('data-src') || '')
         })
     })
 
 }
 
+main()
